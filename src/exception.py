b3fd164e9cf46629ed4fa9f516f3a1cd25566c65
HMourad2023 <hamzaoui.mourad@outlook.com>
2024-03-20 00:08:53 -0400

    EDA and Training model

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -9,8 +9,6 @@ def error_message_detail(error,error_detail:sys):
     error = str(error)
     error_message = "Error occured in Python Script name : [{0}] line number : [{1}] error message : [{2}]".format(filename,lineno,error)
     return error_message
- 
- 
  
 class CustomException(Exception):
      def __init__(self,error_message,error_detail:sys):
--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,6 @@
 import logging
 import os
-import sys
 from datetime import datetime
-from src.exception import CustomException
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logspath = os.path.join(os.getcwd(),"logs",LOG_FILE)

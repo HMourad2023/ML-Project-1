6810605c6246066dcef186f84496c5d10138e2b4
HMourad2023 <hamzaoui.mourad@outlook.com>
2024-03-19 20:40:38 -0400

    logging and exception added

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import sys
+from datetime import datetime
+from src.exception import CustomException
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logspath = os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+os.makedirs(logspath,exist_ok = True)
+
+LOG_FILE_PATH = os.path.join(logspath,LOG_FILE)
+
+logging.basicConfig(
+    
+    filename = LOG_FILE_PATH,
+    format = "[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO
+)
+
+
+    
